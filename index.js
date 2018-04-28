45e7b13b78eed566604c97730fe5e3b830a845e7
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-27 17:14:22 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+const store = {drivers: [], passengers: [], trips: []}
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = ++driverId;
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(trip => { return trip.driverId === this.id});
+  }
+
+  passengers() {
+    return this.trips().map(trip => { return trip.passenger() });
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = ++passengerId;
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(trip => { return trip.passengerId === this.id });
+  }
+
+  drivers() {
+    return this.trips().map(trip => { return trip.driver() });
+  }
+
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = ++tripId;
+    store.trips.push(this);
+  }
+
+  passenger() {
+    return store.passengers.find(passenger => { return passenger.id === this.passengerId});
+  }
+
+  driver() {
+    return store.drivers.find(driver => { return driver.id === this.driverId});
+  }
+}
